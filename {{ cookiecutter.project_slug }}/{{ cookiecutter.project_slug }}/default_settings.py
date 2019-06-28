a9bf975466cf8367f298de3435b9ba6dd1cd7bd8
Tim Savage <tim@savage.company>
2019-06-29 01:02:09 +1000

    Added docs for extensions

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/default_settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/default_settings.py
@@ -6,15 +6,41 @@ DEBUG = True
 # Database Engines
 
 DATABASE_ENGINES = None
+"""
+SQLAlchemy engine connection settings
+
+Example settings::
+
+    DATABASE_ENGINES = {
+        "default": {
+            "url": "driver://user:password@host/database",
+            "connect_args": {
+                # Engine specific args
+            }
+        }
+    }
+
+"""
+
 
 {% endif %}{% if cookiecutter.use_smtp %}
 ##############################################################################
 # SMTP Host
 
-SMTP = {
-    "default": {
-        "host": "hostname",
-    },
-}
+SMTP = {"default": {"host": "localhost"}}
+"""
+SMTP server settings
+
+Example settings::
+
+    SMTP = {
+        "default": {
+            "host": "localhost",
+            "port": 23,
+            "username": None,
+            "password": None,
+        },
+    }
+"""
 
 {% endif %}
