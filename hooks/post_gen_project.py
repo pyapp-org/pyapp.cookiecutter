7842a452bccdc9d8ffb9bcba1c91887a201dd757
Tim Savage <tim@savage.company>
2019-06-29 00:51:57 +1000

    Added test placeholders, hooks and additional params

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+
+PROJECT_DIR = Path().resolve()
+
+
+def remove_file(file_path: str):
+    (PROJECT_DIR / file_path).unlink()
+
+
+if __name__ == "__main__":
+    if "{{ cookiecutter.use_pytest }}" == "y":
+        remove_file("tests/__init__.py")
+    else:
+        remove_file("tests/conftest.py")
+
+    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
+        remove_file('LICENSE')
